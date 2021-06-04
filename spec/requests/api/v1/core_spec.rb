85cec22f77cd2889ef2538be3edf677ad622113a
Norfa Bagas <bgsnorfa@gmail.com>
2021-06-05 00:21:45 +0700

    Add unit test for core controller

--- spec/requests/api/v1/core_spec.rb
+++ spec/requests/api/v1/core_spec.rb
@@ -1,7 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Cores", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /solution" do
+    it "should output correct result" do
+      params = {
+        format: 'json',
+        core: {
+          "name_1": "john",
+          "age_of_death_1": 10,
+          "year_of_death_1": 12,
+          "name_2": "john",
+          "age_of_death_2": 13,
+          "year_of_death_2": 17
+        }
+      }
+
+      post '/api/v1/solution.json', params: params
+      body = JSON.parse(response.body)
+      puts body
+      expect(body['result']).to eq(4.5);
+    end
+
+    it "should output correct result again" do
+      params = {
+        format: 'json',
+        core: {
+          "name_1": "john",
+          "age_of_death_1": 10,
+          "year_of_death_1": 14,
+          "name_2": "john",
+          "age_of_death_2": 10,
+          "year_of_death_2": 15
+        }
+      }
+
+      post '/api/v1/solution.json', params: params
+      body = JSON.parse(response.body)
+      puts body
+      expect(body['result']).to eq(9.5);
+    end
+
+    it "should output -1 as result" do
+      params = {
+        format: 'json',
+        core: {
+          "name_1": "john",
+          "age_of_death_1": 10,
+          "year_of_death_1": 10,
+          "name_2": "john",
+          "age_of_death_2": 10,
+          "year_of_death_2": 10
+        }
+      }
+
+      post '/api/v1/solution.json', params: params
+      body = JSON.parse(response.body)
+      puts body
+      expect(body['result']).to eq(-1);
+    end
   end
 end
